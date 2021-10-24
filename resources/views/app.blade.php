225f87d2b709a0e6ad62bb1091adcbce9867563f
roleqx <wizzsha@gmail.com>
2021-10-24 11:53:21 +0100

    feat: switch to Open Sans font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,14 +8,23 @@
     <link href="{{asset("/css/app.css")}}" rel="stylesheet">
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+    {{--
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> --}}
+    {{--
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> --}}
+    {{--
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,700&display=swap"
+        rel="stylesheet"> --}}
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap" rel="stylesheet">
     <!-- Styles -->
 </head>
 
 <body class="antialiased">
     <div id="app"></div>
+    <div id="app-modal" class="absolute top-0 left-0"></div>
 </body>
-<script script src="/js/app.js"> </script>
+{{-- <script script src="{{mix(" /js/app.js")}}"> </script> --}}
+<script src="{{mix("js/app.js")}}"> </script>
 
 </html>
