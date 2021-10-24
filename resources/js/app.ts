502dd251dc9887d0e484cf53904c07f17bbac557
roleqx <wizzsha@gmail.com>
2021-10-24 11:55:20 +0100

    feat: vue entry file

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,8 +1,9 @@
 // require('./bootstrap');
 import { createApp } from "vue";
 import router from "./src/router";
+import store from "./src/store";
 import App from "./src/App.vue";
 
-const app = createApp(App).use(router);
+const app = createApp(App).use(store).use(router);
 
 app.mount("#app");
