a5a4a38f44723a515fa5cc0c0d4e79acb203ed52
roleqx <wizzsha@gmail.com>
2021-10-24 11:51:14 +0100

    chore: implement vue 3 for frontend

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require("laravel-mix");
-
+const webpack = require("webpack");
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -14,5 +14,25 @@ const mix = require("laravel-mix");
 mix.ts("resources/js/app.ts", "public/js")
     .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
     .vue({ version: 3 })
+    .sourceMaps()
     .disableNotifications()
-    .browserSync({ proxy: { target: "127.0.0.1:8000" }, notify: false });
+    // .browserSync({
+    //     proxy: "http://127.0.0.1:8000",
+    //     host: "http://127.0.0.1",
+    //     port: 8000,
+    //     localOnly: true,
+
+    //     // socket: {
+    //     //     domain: "127.0.0.1:8000",
+    //     // },
+    //     notify: false,
+    // })
+    .webpackConfig({
+        devtool: mix.inProduction() ? false : "inline-source-map",
+        plugins: [
+            new webpack.DefinePlugin({
+                // __VUE_OPTIONS_API__: false,
+                __VUE_PROD_DEVTOOLS__: false,
+            }),
+        ],
+    });
