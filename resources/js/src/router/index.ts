ab8c6011da96f2e21def17ce68c399a9d7626d5e
roleqx <wizzsha@gmail.com>
2021-12-04 07:44:18 +0100

    feat: add router redirect for index route

--- resources/js/src/router/index.ts
+++ resources/js/src/router/index.ts
@@ -1,17 +1,22 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
-import NotFound from "../views/NotFound.vue";
+// import NotFound from "../views/NotFound.vue";
 import Wilayas from "../views/Wilayas.vue";
-import Towns from "../views/Towns.vue";
-import Schedule from "../views/Schedule.vue";
-import ScheduleRefactor from "../views/ScheduleRefactor.vue";
+// import Towns from "../views/Towns.vue";
+// import Schedule from "../views/Schedule.vue";
+// import ScheduleRefactor from "../views/ScheduleRefactor.vue";
+
+const NotFound = () => import("../views/NotFound.vue");
+const Towns = () => import("../views/Towns.vue");
+const Schedule = () => import("../views/Schedule.vue");
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: "/",
         name: "home",
         component: Home,
+        redirect: { name: "wilayas" },
         children: [
             {
                 path: "/wilayas",
@@ -34,19 +39,19 @@ const routes: Array<RouteRecordRaw> = [
                     content: Schedule,
                 },
             },
-            {
-                path: "/wilaya/:wilayaId/town/:townId/schedule/refactored",
-                name: "schedule-ref",
-                components: {
-                    content: ScheduleRefactor,
-                },
-                beforeEnter() {
-                    console.clear();
-                    console.warn(
-                        "[ScheduleRefactor] this route serves as a ground for upgrading the form handling logic to be at vuex level, bringing a harmony between data fetched from the server and the processing of said data at the component level. After the task is complete, this should replace the current schedule route which handles the form at the component level"
-                    );
-                },
-            },
+            // {
+            //     path: "/wilaya/:wilayaId/town/:townId/schedule/refactored",
+            //     name: "schedule-ref",
+            //     components: {
+            //         content: ScheduleRefactor,
+            //     },
+            //     beforeEnter() {
+            //         console.clear();
+            //         console.warn(
+            //             "[ScheduleRefactor] this route serves as a ground for upgrading the form handling logic to be at vuex level, bringing a harmony between data fetched from the server and the processing of said data at the component level. After the task is complete, this should replace the current schedule route which handles the form at the component level"
+            //         );
+            //     },
+            // },
         ],
     },
     {
