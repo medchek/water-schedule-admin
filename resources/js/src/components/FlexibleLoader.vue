0a72ddcfb29bbbf8f7c268edb6d9b89d3c29eae0
roleqx <wizzsha@gmail.com>
2021-12-04 07:25:06 +0100

    feat: add loader component that expands along the whole parent width and height

---
+++ resources/js/src/components/FlexibleLoader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex-grow w-full h-full flex items-center justify-center">
+    <loader :dark="dark" :customColors="customColors" :thin="thin" :className="className" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Loader from "./Loader.vue";
+
+export default defineComponent({
+  components: { Loader },
+  props: {
+    className: {
+      type: String,
+    },
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+    customColors: {
+      type: Boolean,
+      default: false,
+    },
+    thin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
