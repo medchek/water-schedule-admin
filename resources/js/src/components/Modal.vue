8800f13e0e2f57aae7f6e19ac8e2833129f9c5bb
roleqx <wizzsha@gmail.com>
2021-12-13 23:45:30 +0100

    style: use global fade animation

--- resources/js/src/components/Modal.vue
+++ resources/js/src/components/Modal.vue
@@ -97,16 +97,6 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.2s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-
 .scale-enter-active {
   animation: scale 0.25s;
 }
