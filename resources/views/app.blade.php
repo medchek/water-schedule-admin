ec30fc2207c7a5e80f56d76e66ee15ae93fb3af8
roleqx <wizzsha@gmail.com>
2021-12-28 19:55:34 +0100

    feat: add favicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,10 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <title>SEAAL - Program d'eau</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/fav/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/fav/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/fav/favicon-16x16.png">
+    <link rel="shortcut icon" type="image/png" href="/fav/favicon.ico"/>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{asset("/css/app.css")}}" rel="stylesheet">
@@ -21,6 +25,10 @@
     <div id="app-modal" class="absolute top-0 left-0"></div>
 </body>
 {{-- <script script src="{{mix(" /js/app.js")}}"> </script> --}}
+@production
+<script src="js/manifest.js"> </script>
+<script src="js/vendor.js"> </script>
+@endproduction
 <script src="{{mix("js/app.js")}}"> </script>
-
+{{-- <script src="https://www.google.com/recaptcha/api.js" async defer></script> --}}
 </html>
