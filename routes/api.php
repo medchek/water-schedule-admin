97131a5b8b02a9a66df8b644392bfbd9c3b2b7e9
roleqx <wizzsha@gmail.com>
2022-02-09 19:11:03 +0100

    feat: add route for registering the admin user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\PeriodController;
+use App\Http\Controllers\InstallController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,40 +22,49 @@ use App\Http\Resources\UserResource;
 */
 
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return new UserResource($request->user());
-});
+// Route::middleware("auth:api")->get("/user", function (Request $request) {
+// Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
+//     return new UserResource($request->user());
+// });
 
 
-Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(["auth:sanctum"])->group(function () {
 
-    Route::get("/wilayas", [WilayaController::class, 'index']);
-    Route::get("/wilayas/{wilaya_id}", [WilayaController::class, 'show']);
-    Route::get("wilayas/{wilaya_id}/towns/", [TownController::class, 'getByWilayaId']);
+    Route::get("/user", function (Request $request) {
+        return new UserResource($request->user());
+    });
 
-    Route::post("/towns", [TownController::class, 'store']);
-    Route::patch("/towns/{town_id}", [TownController::class, 'update']);
-    Route::delete("/towns/{town_id}", [TownController::class, 'destroy']);
+    Route::get("/wilayas", [WilayaController::class, "index"]);
+    Route::get("/wilayas/{wilaya_id}", [WilayaController::class, "show"]);
+    Route::get("wilayas/{wilaya_id}/towns/", [TownController::class, "getByWilayaId"]);
+
+    Route::post("/towns", [TownController::class, "store"]);
+    Route::patch("/towns/{town_id}", [TownController::class, "update"]);
+    Route::delete("/towns/{town_id}", [TownController::class, "destroy"]);
 
 
-    Route::get("/schedules/{town_id}", [ScheduleController::class, 'show']);
-    Route::post("/schedules", [ScheduleController::class, 'store']);
+    Route::get("/schedules/{town_id}", [ScheduleController::class, "show"]);
+    Route::post("/schedules", [ScheduleController::class, "store"]);
 
     // user settings
-    // TODO: rate limit the amount of reset per hours to 5
-    Route::patch("/settings/reset-password", [UserSettingController::class, 'resetPassword']);
+    // TODO: rate limit the amount of resets per hours to 5
+    Route::patch("/settings/reset-password", [UserSettingController::class, "resetPassword"]);
     // user settings
-    Route::post("/settings/user-settings", [UserSettingController::class, 'storeUserAppSettings']);
-    Route::patch("/settings/user-settings", [UserSettingController::class, 'updateUserAppSettings']);
+    Route::post("/settings/user-settings", [UserSettingController::class, "storeUserAppSettings"]);
+    Route::patch("/settings/user-settings", [UserSettingController::class, "updateUserAppSettings"]);
 });
+
+// register first account routes
+Route::middleware(["throttle:install", "not-auth"])->group(function () {
+    Route::post("/install",  [InstallController::class, "store"])->middleware('can-install');
+    Route::get("/can-install", [InstallController::class, "canInstall"]);
+});
+
 
 // public routes
-
-Route::middleware((['throttle:app-requests']))->group((function () {
-
-    Route::get("/wilayas", [WilayaController::class, 'getPublicWilayas']);
-    Route::get("/towns/{wilaya_code}", [TownController::class, 'publicGetByWilayaId']);
-    Route::get("/schedules/{town_id}/current", [ScheduleController::class, 'getCurrentWeekSchedule']);
-    Route::get("/schedules/{town_id}/next", [ScheduleController::class, 'getNextWeekSchedule']);
+Route::middleware((["throttle:app-requests"]))->prefix("v1")->group((function () {
+    Route::get("/wilayas", [WilayaController::class, "index"]);
+    Route::get("/towns/{wilaya_code}", [TownController::class, "getByWilayaId"]);
+    Route::get("/schedules/{town_id}/current", [ScheduleController::class, "getCurrentWeekSchedule"]);
+    Route::get("/schedules/{town_id}/next", [ScheduleController::class, "getNextWeekSchedule"]);
 }));
