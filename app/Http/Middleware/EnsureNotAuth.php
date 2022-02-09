2e7bd1ab329bb24b2306719d1be1f2fb7cd3bc74
roleqx <wizzsha@gmail.com>
2022-02-09 20:04:58 +0100

    feat: add middleware to assert the user is unauthenticated
    this middleware is used to check if the user is not already logged at the account creation process

---
+++ app/Http/Middleware/EnsureNotAuth.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class EnsureNotAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::guest()) {
+
+            return $next($request);
+        } else {
+            return response("already logged", Response::HTTP_FORBIDDEN);
+        }
+    }
+}
