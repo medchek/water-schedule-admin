e491a9e53fb118f7d2bce9fed8c4a586b6be30f1
roleqx <wizzsha@gmail.com>
2022-02-11 18:41:09 +0100

    feat: add observer to prevent admin account creation after the first one is added

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use App\Services\UserService;
+use Illuminate\Support\Facades\Cache;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        // if the admin user is created, forget the can-install cache to reflect the changes
+        if ($user->account_level === UserService::ACCOUNT_LEVEL_ADMIN) {
+            Cache::forget("can-install");
+        }
+    }
+
+    /**
+     * Handle the User "updated" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
