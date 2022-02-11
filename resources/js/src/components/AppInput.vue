6cfc0b94d7576b3906e2d88e4510e0187a0be5b8
roleqx <wizzsha@gmail.com>
2022-02-11 19:48:24 +0100

    feat: add id and autocomplete attributes to input component

--- resources/js/src/components/AppInput.vue
+++ resources/js/src/components/AppInput.vue
@@ -1,13 +1,15 @@
 <template>
   <div :class="`${className ? className : 'w-80 h-10'} flex items-center relative`">
     <input
+      :id="inputId"
       :type="type"
       :class="[
         appendIcon ? 'pl-9 arabic:pr-9' : 'pl-2 arabic:pr-2',
-        `w-full h-full border border-gray-200 focus:ring-2 ring-blue-200 dark:ring-indigo-500 rounded-md placeholder-gray-300 dark:placeholder-bgray-400`,
+        `w-full h-full border border-gray-200 focus:ring-2 ring-blue-300 dark:ring-indigo-500 rounded-md placeholder-gray-300 dark:placeholder-bgray-400`,
       ]"
       :placeholder="placeholder"
       :value="modelValue"
+      :autocomplete="autocomplete"
       @input="$emit('update:modelValue', $event.target.value)"
     />
     <icon v-if="appendIcon" :icon="appendIcon" class="absolute left-2 arabic:right-2 arabic:left-auto text-gray-300 h-6 w-6 dark:text-bgray-400" />
@@ -48,10 +50,13 @@ export default defineComponent({
     placeholder: {
       type: String,
     },
+    autocomplete: String,
+    value: String,
     type: {
       type: String,
       default: "text",
     },
+    inputId: String,
   },
   setup() {
     return { mdiClose };
