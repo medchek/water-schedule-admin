db36be359e6620c54935b15ef5718f580ce342d2
CH MD <39788804+medchek@users.noreply.github.com>
2022-02-15 19:41:53 +0100

    fix: remove unused logger facade

--- app/Actions/RecaptchaVerification.php
+++ app/Actions/RecaptchaVerification.php
@@ -4,12 +4,14 @@ namespace App\Actions;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use ReCaptcha\ReCaptcha as GoogleRecaptcha;
 
 class RecaptchaVerification
 {
   public function __invoke(Request $request, Closure $next)
   {
+
     $recaptcha = new GoogleRecaptcha(env("RECAPTCHA_SECRET_KEY"));
     $res = $recaptcha->setExpectedHostname(env("SESSION_DOMAIN"))
       ->verify($request["g-recaptcha-response"], $request->ip());
