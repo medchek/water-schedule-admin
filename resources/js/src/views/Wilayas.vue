ee62f3a208bff5162c3fc1dca4d0e0ba11494a5d
CH MD <39788804+medchek@users.noreply.github.com>
2022-02-16 18:31:59 +0100

    fix: missing unlocalized message

--- resources/js/src/views/Wilayas.vue
+++ resources/js/src/views/Wilayas.vue
@@ -1,56 +1,42 @@
 <template>
-  <view-container :label="t('general.wilayas')">
-    <section
-      id="towns-toolbar"
-      class="flex flex-col lg:flex-row lg:arabic:flex-row-reverse lg:items-center justify-end lg:h-14 w-full mb-2 px-5 space-y-2 lg:space-y-0"
-    >
-      <app-input
-        spellcheck="false"
-        maxlength="50"
-        className="h-10 text-sm sm:text-base w-full lg:w-80 "
-        v-model.trim.lazy="searchTerm"
-        :appendIcon="mdiMagnify"
-        :placeholder="t('wilaya.searchForWilaya')"
-        clearable
-      />
-    </section>
-    <section id="content-main" class="relative w-full h-full grow overflow-y-auto py-2 my-2">
-      <load-and-retry
-        v-if="isFetching || fetchingError"
-        @retry="fetchWilayas"
-        :hasFailed="fetchingError"
-        :isFetching="isFetching"
-        :text="t('wilaya.errorFetching')"
-      />
-      <!-- SUCCESS -->
+    <view-container :label="t('general.wilayas')">
+        <section
+            id="towns-toolbar"
+            class="flex flex-col lg:flex-row lg:arabic:flex-row-reverse lg:items-center justify-end lg:h-14 w-full mb-2 px-5 space-y-2 lg:space-y-0"
+        >
+            <app-input
+                spellcheck="false"
+                maxlength="50"
+                className="h-10 text-sm sm:text-base w-full lg:w-80 "
+                v-model.trim.lazy="searchTerm"
+                :appendIcon="mdiMagnify"
+                :placeholder="t('wilaya.searchForWilaya')"
+                clearable
+            />
+        </section>
+        <section id="content-main" class="relative w-full h-full grow overflow-y-auto py-2 my-2">
+            <load-and-retry
+                v-if="isFetching || fetchingError"
+                @retry="fetchWilayas"
+                :hasFailed="fetchingError"
+                :isFetching="isFetching"
+                :text="t('wilaya.errorFetching')"
+            />
+            <!-- SUCCESS -->
 
-      <div
-        class="
-          relative
-          grid
-          arabic:direction-rtl
-          grid-cols-1
-          sm:grid-cols-2
-          lg:grid-cols-3
-          xl:grid-cols-4
-          2xl:grid-cols-5
-          grid-rows-none
-          gap-4
-          w-full
-          h-auto
-          px-5
-        "
-        v-if="!isFetching && !fetchingError && wilayas"
-      >
-        <wilaya-card v-for="wilaya in wilayas" :wilaya="wilaya" :key="wilaya.code" />
-      </div>
-      <!-- SUCCESS BUT NO SEARCH RESULT -->
+            <div
+                class="relative grid arabic:direction-rtl grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 grid-rows-none gap-4 w-full h-auto px-5"
+                v-if="!isFetching && !fetchingError && wilayas"
+            >
+                <wilaya-card v-for="wilaya in wilayas" :wilaya="wilaya" :key="wilaya.code" />
+            </div>
+            <!-- SUCCESS BUT NO SEARCH RESULT -->
 
-      <div v-if="!isFetching && !fetchingError && searchTerm.length && !wilayas.length" class="w-full text-center text-lg text-bgray-700 font-bold">
-        <p>Aucune wilaya n'a été ne correspond à votre recherche.</p>
-      </div>
-    </section>
-  </view-container>
+            <div v-if="!isFetching && !fetchingError && searchTerm.length && !wilayas.length" class="w-full text-center text-lg text-bgray-700 font-bold">
+                <p>{{ t("wilaya.noSearchMatch") }}.</p>
+            </div>
+        </section>
+    </view-container>
 </template>
 <script lang="ts">
 import { computed, defineComponent, onMounted, ref, ComputedRef } from "vue";
@@ -67,40 +53,40 @@ import { mdiMagnify } from "@mdi/js";
 import { useI18n } from "vue-i18n";
 
 export default defineComponent({
-  components: { Loader, WilayaCard, LoadAndRetry: Retry, AppInput, ViewContainer },
-  setup() {
-    const { t } = useI18n();
-    const store = useStore();
-    const isFetching = ref<boolean>(false);
-    const fetchingError = ref<boolean>(false);
+    components: { Loader, WilayaCard, LoadAndRetry: Retry, AppInput, ViewContainer },
+    setup() {
+        const { t } = useI18n();
+        const store = useStore();
+        const isFetching = ref<boolean>(false);
+        const fetchingError = ref<boolean>(false);
 
-    const searchTerm = ref<string>("");
-    const isAuth = computed(() => store.getters.getUser);
+        const searchTerm = ref<string>("");
+        const isAuth = computed(() => store.getters.getUser);
 
-    const allWilayas: ComputedRef<Wilaya[]> = computed(() => store.getters.getWilayas);
-    const wilayas: ComputedRef<Wilaya[]> = computed(() => store.getters.getFilteredWilayas(searchTerm.value));
+        const allWilayas: ComputedRef<Wilaya[]> = computed(() => store.getters.getWilayas);
+        const wilayas: ComputedRef<Wilaya[]> = computed(() => store.getters.getFilteredWilayas(searchTerm.value));
 
-    const fetchWilayas = () => {
-      isFetching.value = true;
-      if (fetchingError.value) fetchingError.value = false;
-      store
-        .dispatch("fetchWilayas")
-        .catch(() => {
-          if (!fetchingError.value) fetchingError.value = true;
-        })
-        .finally(() => {
-          isFetching.value = false;
+        const fetchWilayas = () => {
+            isFetching.value = true;
+            if (fetchingError.value) fetchingError.value = false;
+            store
+                .dispatch("fetchWilayas")
+                .catch(() => {
+                    if (!fetchingError.value) fetchingError.value = true;
+                })
+                .finally(() => {
+                    isFetching.value = false;
+                });
+        };
+        onMounted(() => {
+            if (isAuth === null) return;
+            // if wilayas have already been loaded from the server
+            if (allWilayas.value.length) return;
+            // otherwise, fetch for the wilayas
+            fetchWilayas();
         });
-    };
-    onMounted(() => {
-      if (isAuth === null) return;
-      // if wilayas have already been loaded from the server
-      if (allWilayas.value.length) return;
-      // otherwise, fetch for the wilayas
-      fetchWilayas();
-    });
 
-    return { fetchWilayas, isFetching, fetchingError, wilayas, searchTerm, mdiMagnify, t };
-  },
+        return { fetchWilayas, isFetching, fetchingError, wilayas, searchTerm, mdiMagnify, t };
+    },
 });
 </script>
