caffaa072be26e14229be1ccc844c851b298b551
CH MD <39788804+medchek@users.noreply.github.com>
2022-02-20 20:13:03 +0100

    chore: add deployment tasks

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: vendor/bin/heroku-php-apache2 public/
+release: ./deploy-tasks.sh
---
+++ deploy-tasks.sh
@@ -0,0 +1,7 @@
+php artisan config:clear 
+php artisan route:clear 
+php artisan view:clear
+php artisan migrate 
+php artisan config:cache 
+php artisan route:cache
+php artisan view:cache
