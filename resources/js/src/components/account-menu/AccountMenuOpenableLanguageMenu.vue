fac589481e7e55b8965aa2f2146b65951b5f670c
CH MD <39788804+medchek@users.noreply.github.com>
2022-02-22 17:43:38 +0100

    feat: add loader while language files are loading

--- resources/js/src/components/account-menu/AccountMenuOpenableLanguageMenu.vue
+++ resources/js/src/components/account-menu/AccountMenuOpenableLanguageMenu.vue
@@ -1,47 +1,87 @@
 <template>
-  <section>
-    <account-menu-openable-item text="Français" @click="setLangFr" :selected="selectedLanguage == 'fr'" />
-    <account-menu-openable-item text="العربية" @click="setLangAr" :selected="selectedLanguage == 'ar'" />
-  </section>
+    <section>
+        <account-menu-openable-item
+            text="Français"
+            @click="setLangFr"
+            :selected="selectedLanguage == 'fr'"
+            :isLoading="isLangLoading == 'fr'"
+        />
+        <account-menu-openable-item
+            text="العربية"
+            @click="setLangAr"
+            :selected="selectedLanguage == 'ar'"
+            :isLoading="isLangLoading == 'ar'"
+        />
+    </section>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref } from "vue";
 import { mdiBrightness5, mdiBrightness4 } from "@mdi/js";
 import AccountMenuOpenableItem from "./AccountMenuOpenableItem.vue";
 import { useStore } from "vuex";
 import { useI18n } from "vue-i18n";
+import { AvailableLocales, loadLocaleMessages } from "../../locales";
+import { flashErrorSnack } from "../../lib/shared";
 
 export default defineComponent({
-  components: { AccountMenuOpenableItem },
-  emits: ["selected"],
-  setup(_, { emit }) {
-    const { locale } = useI18n({ useScope: "global" });
-    const store = useStore();
-    const selectedLanguage = computed(() => store.getters.getLanguage);
-    const setLangFr = () => {
-      if (locale.value !== "fr") {
-        locale.value = "fr";
-        store.dispatch("setLanguageFr");
-        emit("selected");
-      }
-    };
-    const setLangAr = () => {
-      if (locale.value !== "ar") {
-        locale.value = "ar";
-        store.dispatch("setLanguageAr");
-        emit("selected");
-      }
-    };
+    components: { AccountMenuOpenableItem },
+    emits: ["selected"],
+    setup(_, { emit }) {
+        const { locale, t } = useI18n({ useScope: "global" });
+        const store = useStore();
+        const selectedLanguage = computed(() => store.getters.getLanguage);
 
-    return {
-      selectedLanguage,
-      setLangFr,
-      setLangAr,
-      // icons
-      mdiBrightness5,
-      mdiBrightness4,
-    };
-  },
+        const isLangLoading = ref<AvailableLocales | null>(null);
+        //
+        const setLangFr = async () => {
+            if (locale.value !== "fr") {
+                await loadLang("fr", () => {
+                    locale.value = "fr";
+                    store.dispatch("setLanguageFr");
+                    emit("selected");
+                });
+            }
+        };
+
+        //
+        const setLangAr = async () => {
+            if (locale.value !== "ar") {
+                await loadLang("ar", () => {
+                    locale.value = "ar";
+                    store.dispatch("setLanguageAr");
+                    emit("selected");
+                });
+            }
+        };
+
+        /**
+         * Lazy loads the lang files and runs the callback function after it's successfully loaded
+         * @param locale the target locale
+         * @param callback the callback function to run after the language json is loaded
+         */
+        const loadLang = async (locale: AvailableLocales, callback: () => void) => {
+            try {
+                isLangLoading.value = locale;
+                await loadLocaleMessages(locale);
+                callback();
+            } catch {
+                flashErrorSnack(t("general.snack.errors.langImport"));
+                throw new Error("could not load ar language data");
+            } finally {
+                isLangLoading.value = null;
+            }
+        };
+
+        return {
+            selectedLanguage,
+            setLangFr,
+            setLangAr,
+            isLangLoading,
+            // icons
+            mdiBrightness5,
+            mdiBrightness4,
+        };
+    },
 });
 </script>
