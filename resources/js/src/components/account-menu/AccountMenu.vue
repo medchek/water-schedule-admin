36449c8920cf56c5be134c32b9e64c288ded1bb8
CH MD <39788804+medchek@users.noreply.github.com>
2022-02-22 15:55:43 +0100

    feat: add loading component while the dropdown menu is lazy-loaded

--- resources/js/src/components/account-menu/AccountMenu.vue
+++ resources/js/src/components/account-menu/AccountMenu.vue
@@ -1,37 +1,43 @@
 <template>
-  <div id="account-menu" class="relative">
-    <button
-      id="open-account-menu-button"
-      class="w-10 h-10 rounded-full bg-gray-200 active:bg-gray-100 text-bgray-500 hover:border-2 border-blue-300"
-      :class="[!forceLight && 'dark:bg-dark-alt dark:text-bgray-200']"
-      @mousedown.prevent.capture="toggleMenu"
-    >
-      <icon :icon="mdiAccountOutline" className="w-7 h-7 pointer-events-none" />
-    </button>
-    <account-menu-dropdown v-if="isOpen" @clickedOutside="closeMenu" @close="closeMenu" />
-  </div>
+    <div id="account-menu" class="relative">
+        <button
+            id="open-account-menu-button"
+            class="w-10 h-10 rounded-full bg-gray-200 active:bg-gray-100 text-bgray-500 hover:border-2 border-blue-300"
+            :class="[!forceLight && 'dark:bg-dark-alt dark:text-bgray-200']"
+            @mousedown.prevent.capture="toggleMenu"
+        >
+            <icon :icon="mdiAccountOutline" className="w-7 h-7 pointer-events-none" />
+        </button>
+        <account-menu-dropdown v-if="isOpen" @clickedOutside="closeMenu" @close="closeMenu" />
+    </div>
 </template>
 
 <script lang="ts">
 import { defineAsyncComponent, defineComponent, ref } from "vue";
 import { mdiAccountOutline } from "@mdi/js";
 import Icon from "../Icon.vue";
+import AccountMenuDropdownLoader from "./AccountMenuDropdownLoader.vue";
 
 export default defineComponent({
-  props: {
-    forceLight: {
-      type: Boolean,
-      default: false,
+    props: {
+        forceLight: {
+            type: Boolean,
+            default: false,
+        },
     },
-  },
-  components: { Icon, AccountMenuDropdown: defineAsyncComponent({ loader: () => import("./AccountMenuDropdown.vue") }) },
-  setup() {
-    const isOpen = ref(false);
-    const toggleMenu = () => (isOpen.value = !isOpen.value);
-    const closeMenu = () => (isOpen.value = false);
-    const accountMenuRef = ref<HTMLElement | null>(null);
+    components: {
+        Icon,
+        AccountMenuDropdown: defineAsyncComponent({
+            loader: () => import("./AccountMenuDropdown.vue"),
+            loadingComponent: AccountMenuDropdownLoader,
+        }),
+    },
+    setup() {
+        const isOpen = ref(false);
+        const toggleMenu = () => (isOpen.value = !isOpen.value);
+        const closeMenu = () => (isOpen.value = false);
 
-    return { isOpen, toggleMenu, closeMenu, accountMenuRef, mdiAccountOutline };
-  },
+        return { isOpen, toggleMenu, closeMenu, mdiAccountOutline };
+    },
 });
 </script>
