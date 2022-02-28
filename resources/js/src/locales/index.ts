8d2baacf1ad6114143db7fde5c00bc038f1f29b8
CH MD <39788804+medchek@users.noreply.github.com>
2022-02-28 10:41:07 +0100

    feat: update locale names

--- resources/js/src/locales/index.ts
+++ resources/js/src/locales/index.ts
@@ -1,3 +1,4 @@
+import { env } from "./../lib/shared";
 import { createI18n } from "vue-i18n";
 // import frFr from "./fr-Fr.json";
 // import arDz from "./ar-Dz.json";
@@ -6,7 +7,7 @@ import { nextTick } from "vue";
 
 // type MessageSchema = typeof frFr;
 
-type AvailableLocalesFiles = "fr-Fr" | "ar-Dz";
+type AvailableLocalesFiles = "fr-FR" | "ar-DZ";
 export type AvailableLocales = "fr" | "ar";
 
 const messages = {
@@ -25,13 +26,19 @@ const i18n = createI18n({
 
 // locale lazy loading
 export async function loadLocaleMessages(locale: AvailableLocales) {
+    if (env(process.env.MIX_APP_DEBUG, "false") == "true") console.log("Loading locales");
+
+    // if the locales have already been loaded, dont load them again
     if (i18n.global.availableLocales.includes(locale)) return;
 
-    const localeFileName: AvailableLocalesFiles = locale === "ar" ? "ar-Dz" : "fr-Fr";
+    const localeFileName: AvailableLocalesFiles = locale === "ar" ? "ar-DZ" : "fr-FR";
     // load locale messages with dynamic import
     const messages = await import(
         /* webpackChunkName: "locale-[request]" */ `./${localeFileName}.json`
     );
+    if (env(process.env.MIX_APP_DEBUG, "false") == "true") {
+        console.log("locales successfully imported", messages);
+    }
 
     // set locale and locale message
     i18n.global.setLocaleMessage(locale, messages.default);
